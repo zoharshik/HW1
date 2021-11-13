9101eb5b63c456707fab10d56cecfb4e670ad329
zohar shikler <zoharshik@gmail.com>
2021-11-13 20:49:36 +0200

    final changes

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,5 +1,62 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank implements IBank{
+    public List<IAccount> Accounts;
+
+    public Bank(){
+        this.Accounts = new ArrayList<>();
+
+    }
+
+    @Override
+    public void OpenAccount(IAccount account) {
+        this.Accounts.add(account);
+    }
+
+    @Override
+    public void CloseAccount(int accountNumber) {
+        for (IAccount x : this.Accounts){
+            if(x.GetAccountNumber() == accountNumber){
+                if(x.GetCurrentBalance() >= 0){
+                    this.Accounts.remove(x);
+                }else{
+                    System.out.println("this account cannot be closed due to debt");
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public List<IAccount> GetAllAccount() {
+        return this.Accounts;
+    }
+
+    @Override
+    public List<IAccount> GetAllAccountInDebt() {
+        List<IAccount> temp = new ArrayList<>();
+        for(IAccount x : this.Accounts){
+            if(x.GetCurrentBalance() < 0){
+                temp.add(x);
+            }
+        }
+        return temp;
+    }
+
+    @Override
+    public List<IAccount> GetAllAccountWithBalance(double balanceAbove) {
+        List<IAccount> temp = new ArrayList<>();
+        for(IAccount x : this.Accounts){
+            if(x.GetCurrentBalance() > balanceAbove){
+                temp.add(x);
+            }
+        }
+        return temp;
+
+    }
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        BasicAccount x = new BasicAccount(222,300);
+        IAccount x = new BasicAccount(222,300);
         x.Deposit(400);
         double y = x.Withdraw(500);
         System.out.println(y);
-        System.out.println(x.Balance);
         double z = x.GetCurrentBalance();
         int w = x.GetAccountNumber();
         System.out.println(z);
